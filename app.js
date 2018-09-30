a5d8635019c1f15b5c95b120bb0283bc06b42214
Ahmad Saeed <asaeed@gmail.com>
2018-09-30 13:20:48 -0400

    image classifier

--- app.js
+++ app.js
@@ -2,26 +2,35 @@
 let mobileNet;
 let tiger;
 
+function preload() {
+    //tiger = createImg('img/tiger.jpg');
+}
+
 function setup() {
-    createCanvas(640, 480);
+    createCanvas(1400/2, 697/2);
     background(0);
 
     tiger = createImg('img/tiger.jpg', () => {
-        image(tiger, 0, 0, width, height);
+        image(tiger, 0, 0, tiger.width/2, tiger.height/2);
     });
     tiger.hide();
 
 
 
-    mobileNet = ml5.imageClassifier('MobileNet', () => {
+    ml5.imageClassifier('MobileNet').then((classifier) => {
         console.log('model ready');
-
-        mobileNet.predict(tiger, (err, results) => {
-            if (err) {
-                console.error(err);
-            } else {
-                console.log(results)
-            }
-        });
+        mobileNet = classifier;
+        return mobileNet.predict(tiger)
+    })
+    .then(results => {
+        console.log(results);
+        fill(0);
+        textSize(64);
+        text(results[0].className, 10, height-100);
+        createP(results[0].probability);
+    })
+    .catch(err => {
+        console.log(err);
     });
+
 }
